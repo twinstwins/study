2ae0f314de9ec99b51108bf4b7a9b4172855285e
nakamura <weto2kakokako@yahoo.co.jp>
2022-03-30 03:03:32 +0900

    環境変数を返却するサンプルレスポンスを作成

--- project/golang_react_todo_app/go-app/src/main.go
+++ project/golang_react_todo_app/go-app/src/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
 )
 
 // タスクを定義
-type album struct {
+type task struct {
     ID string  `json:"id"`
     name  string  `json:"name"`
     isFinished string  `json:"is_finished"`
@@ -21,13 +25,31 @@ func main() {
 
     // ルーティング
      router.GET("api/ping", testResponse)
+     
+     
+     router.GET("api/env", envResponse)
+
 
     // ルーターを起動して 8000 でリクエストを待つ
     router.Run(":8000")
 }
 
+
 func testResponse(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
             "message": "ping テスト用のレスポンス返却でいい",
+        })
+}
+
+func envResponse(c *gin.Context) {
+    // 環境変数を読み込む
+	err := godotenv.Load(".env")
+    if err != nil {
+        fmt.Printf("読み込み出来ませんでした: %v", err)
+	} 
+
+    message := os.Getenv("SAMPLE_MESSAGE")
+    c.JSON(http.StatusOK, gin.H{
+            "env": message,
         })
 }
