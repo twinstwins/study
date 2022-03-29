2bc01fad05fd14364a139c583d3dbe8c68fb02b1
nakamura <weto2kakokako@yahoo.co.jp>
2022-03-30 02:13:47 +0900

    ルーティングをリファクタリング

--- project/golang_react_todo_app/go-app/src/main.go
+++ project/golang_react_todo_app/go-app/src/main.go
@@ -1,13 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// タスクを定義
+type album struct {
+    ID string  `json:"id"`
+    name  string  `json:"name"`
+    isFinished string  `json:"is_finished"`
+    priority_num  int `json:"priority_num"`
+}
+
 
 func main() {
-    r := gin.Default()
-    r.GET("/api/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "ping テスト用のレスポンス返却でい",
+    // ルーターの初期化
+    router := gin.Default()
+
+    // ルーティング
+     router.GET("api/ping", testResponse)
+
+    // ルーターを起動して 8000 でリクエストを待つ
+    router.Run(":8000")
+}
+
+func testResponse(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+            "message": "ping テスト用のレスポンス返却でいい",
         })
-    })
-    r.Run(":8000")
 }
