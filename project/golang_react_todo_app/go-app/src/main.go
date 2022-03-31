7d8dc840ebba33fcd2e939ad9c529071ab611ad5
nakamura <weto2kakokako@yahoo.co.jp>
2022-03-31 10:59:14 +0900

    1件取得のapi作成

--- project/golang_react_todo_app/go-app/src/main.go
+++ project/golang_react_todo_app/go-app/src/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 )
@@ -15,12 +18,49 @@ func main() {
     // ルーターの初期化
     router := gin.Default()
 
+
+ router.Use(cors.New(cors.Config{
+        // 許可したいHTTPメソッドの一覧
+        AllowMethods: []string{
+            "POST",
+            "GET",
+            "OPTIONS",
+            "PUT",
+            "DELETE",
+        },
+        // 許可したいHTTPリクエストヘッダの一覧
+        AllowHeaders: []string{
+            "Access-Control-Allow-Headers",
+            "Content-Type",
+            "Content-Length",
+            "Accept-Encoding",
+            "X-CSRF-Token",
+            "Authorization",
+        },
+        // 許可したいアクセス元の一覧
+        AllowOrigins: []string{
+            "http://localhost:80",
+        },
+        // 自分で許可するしないの処理を書きたい場合は、以下のように書くこともできる
+        // AllowOriginFunc: func(origin string) bool {
+        //  return origin == "https://www.example.com:8080"
+        // },
+        // preflight requestで許可した後の接続可能時間
+        // https://godoc.org/github.com/gin-contrib/cors#Config の中のコメントに詳細あり
+        MaxAge: 24 * time.Hour,
+    }))
+
+
     // ルーティング
      router.GET("api/ping", testResponse)
-     
      router.GET("api/env", envResponse)
-     
      router.GET("api/db", dbResponse)
+     
+     router.GET("api/task/:id", getTask)
+    //  router.POST("/somePost", postTask)
+    //  router.PUT("/somePut", PutTask)
+    //  router.DELETE("/someDelete", DeleteTask)
+
 
     // ルーターを起動して 8000 でリクエストを待つ
     router.Run(":8000")
@@ -56,13 +96,37 @@ type Task struct {
     isFinished bool  `gorm:"column:age"`
 }
 
+
+func getTask(c *gin.Context){
+    db := gormConnect()
+
+    id := c.Param("id")
+    db.Where("ID = ?", id)
+
+    task := Task{}
+    db.Where("ID = ?", id).First(&task)
+    c.JSON(http.StatusOK, task)
+}
+
 // db接続テスト
 func dbResponse(c *gin.Context) {
-    // 環境変数読み込み
+    db := gormConnect()
+
+    // TODO: dbの設定情報を取得して返す
+    var users []Task
+    db.Find(&users)
+    defer db.Close()
+
+    c.JSON(http.StatusOK, gin.H{
+            "tasks": &users,})
+}
+
+func gormConnect() *gorm.DB {
+     // 環境変数読み込み
     err := godotenv.Load(".env")
     if err != nil {
         fmt.Printf("読み込み出来ませんでした: %v", err)
-	} 
+    } 
 
     // DB接続設定
     DB_USER := os.Getenv("DB_USER")
@@ -74,19 +138,11 @@ func dbResponse(c *gin.Context) {
 
     // DBに接続
     db, err := gorm.Open("mysql", CONNECT)
+
     if err != nil {
         panic(err.Error())
-        c.JSON(http.StatusBadRequest, gin.H{
-            "message": "接続失敗です",
-        })
     }
 
-    var users []Task
-    db.Find(&users)
-    defer db.Close()
-
-    c.JSON(http.StatusOK, gin.H{
-            "tasks": &users,
-        })
+     fmt.Println("db connected: ", &db)
+    return db
 }
-
