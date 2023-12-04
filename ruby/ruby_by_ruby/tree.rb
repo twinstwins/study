6d028cbb4874a555833e8223bb0ba9ee02b195ec
nakamura <weto2kakokako@yahoo.co.jp>
2023-12-04 14:57:01 +0900

    全ての木を表示する関数を定義

--- ruby/ruby_by_ruby/tree.rb
+++ ruby/ruby_by_ruby/tree.rb
@@ -8,3 +8,14 @@ p tree
 # 節1の左の枝を表示する
 p tree[1]
 
+# 木の名前全てを表示する 
+# 再帰的に木をたどる
+def preorder(tree)
+    p(tree[0])
+    if tree[0].start_with?("節")
+        preorder(tree[1])
+        preorder(tree[2])
+    end
+end
+
+preorder(tree)
