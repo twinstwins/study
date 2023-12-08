9c73771ff173e3f19b99f36681680c67b6344be3
nakamura <weto2kakokako@yahoo.co.jp>
2023-12-08 13:30:27 +0900

    変数の代入と参照を定義

--- ruby/ruby_by_ruby/calculator.rb
+++ ruby/ruby_by_ruby/calculator.rb
@@ -1,35 +1,40 @@
 require "./minruby.rb"
 
-def evaluate(tree)
+env = {}
+def evaluate(tree, env)
     case tree[0] 
     when "lit"
         tree[1]
     when  "+"
-        evaluate(tree[1]) + evaluate(tree[2])
+        evaluate(tree[1], env) + evaluate(tree[2], env)
     when "-"
-        evaluate(tree[1]) - evaluate(tree[2])
+        evaluate(tree[1], env) - evaluate(tree[2], env)
     when "*"
-        evaluate(tree[1]) * evaluate(tree[2])
+        evaluate(tree[1], env) * evaluate(tree[2], env)
     when "/"
-        evaluate(tree[1]) / evaluate(tree[2])
+        evaluate(tree[1], env) / evaluate(tree[2], env)
     when "%"
-        evaluate(tree[1]) % evaluate(tree[2])
+        evaluate(tree[1], env) % evaluate(tree[2], env)
     when "**"
-        evaluate(tree[1]) ** evaluate(tree[2])
+        evaluate(tree[1], env) ** evaluate(tree[2], env)
+    when "var_assign"
+        env[tree[1]] = evaluate(tree[2], env)
+    when "var_ref"
+        env[tree[1]]
     when "stmts"
         i = 1
         last = nil
         while tree[i] != nil
-            last = evaluate(tree[i])
+            last = evaluate(tree[i], env)
             i = i + 1
         end
         last # 最後に評価した式の値を返す。プログラム的にはあまり意味はないが複文の値を返すという点では意味がある
     when "func_call"
-        p(evaluate(tree[2]))
+        p(evaluate(tree[2], env))
     end
 end
 
 
 str = minruby_load()
 tree = minruby_parse(str)
-answer = evaluate(tree)
+answer = evaluate(tree, env)
