8a96ad98624f1009f2db54275146c4818de6e54d
nakamura <weto2kakokako@yahoo.co.jp>
2023-12-08 20:31:25 +0900

    WIP: 組み込み関数定義

--- ruby/ruby_by_ruby/calculator.rb
+++ ruby/ruby_by_ruby/calculator.rb
@@ -1,52 +1,57 @@
 require "./minruby.rb"
 
-env = {}
-def evaluate(tree, env)
+# このハッシュテーブルは、変数名をキーとして、その変数の値を格納する
+lenv = {}
+# このハッシュテーブルは、関数名をキーとして、その関数の本体を格納する
+genv ={"p" => ["builtin", "p"]
+}
+
+def evaluate(tree, lenv)
     case tree[0] 
     when "lit"
         tree[1]
     when "+"
-        evaluate(tree[1], env) + evaluate(tree[2], env)
+        evaluate(tree[1], lenv) + evaluate(tree[2], lenv)
     when "-"
-        evaluate(tree[1], env) - evaluate(tree[2], env)
+        evaluate(tree[1], lenv) - evaluate(tree[2], lenv)
     when "*"
-        evaluate(tree[1], env) * evaluate(tree[2], env)
+        evaluate(tree[1], lenv) * evaluate(tree[2], lenv)
     when "/"
-        evaluate(tree[1], env) / evaluate(tree[2], env)
+        evaluate(tree[1], lenv) / evaluate(tree[2], lenv)
     when "%"
-        evaluate(tree[1], env) % evaluate(tree[2], env)
+        evaluate(tree[1], lenv) % evaluate(tree[2], lenv)
     when "**"
-        evaluate(tree[1], env) ** evaluate(tree[2], env)
+        evaluate(tree[1], lenv) ** evaluate(tree[2], lenv)
     when "=="
-        evaluate(tree[1], env) == evaluate(tree[2], env)
+        evaluate(tree[1], lenv) == evaluate(tree[2], lenv)
     when ">"
-        evaluate(tree[1], env) > evaluate(tree[2], env)
+        evaluate(tree[1], lenv) > evaluate(tree[2], lenv)
     when "<"
-        evaluate(tree[1], env) < evaluate(tree[2], env)
+        evaluate(tree[1], lenv) < evaluate(tree[2], lenv)
     when "if"
-        if evaluate(tree[1], env)
-            evaluate(tree[2], env)
+        if evaluate(tree[1], lenv)
+            evaluate(tree[2], lenv)
         else
-            evaluate(tree[3], env)
+            evaluate(tree[3], lenv)
         end
     when "while"
-        while evaluate(tree[1], env)
-            evaluate(tree[2], env)
+        while evaluate(tree[1], lenv)
+            evaluate(tree[2], lenv)
         end
         when "var_assign"
-        env[tree[1]] = evaluate(tree[2], env)
+        lenv[tree[1]] = evaluate(tree[2], lenv)
     when "var_ref"
-        env[tree[1]]
+        lenv[tree[1]]
     when "stmts"
         i = 1
         last = nil
         while tree[i] != nil
-            last = evaluate(tree[i], env)
+            last = evaluate(tree[i], lenv)
             i = i + 1
         end
         last # 最後に評価した式の値を返す。プログラム的にはあまり意味はないが複文の値を返すという点では意味がある
     when "func_call"
-        p(evaluate(tree[2], env))
+        p(evaluate(tree[2], lenv))
     
     else
         pp(tree)
@@ -57,4 +62,4 @@ end
 
 str = minruby_load()
 tree = minruby_parse(str)
-answer = evaluate(tree, env)
+answer = evaluate(tree, lenv)
